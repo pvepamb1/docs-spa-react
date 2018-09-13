1102ad53850906b60f5c62ebfeadabf0388932c1
Prasenna Venkatesh Vepambedu <prasennavenkatesh@gmail.com>
2018-09-13 05:58:30 -0400

    NoiseWords added to set and Contents added to Map

--- work/prj1-sol/doc-finder.js
+++ work/prj1-sol/doc-finder.js
@@ -1,70 +1,75 @@
 const {inspect} = require('util'); //for debugging
+let set;
 
 'use strict';
 
 class DocFinder {
 
-  /** Constructor for instance of DocFinder. */
-  constructor() {
-    //@TODO
-  }
+    /** Constructor for instance of DocFinder. */
+    constructor() {
+        this.contents = new Map();
+    }
 
-  /** Return array of non-noise normalized words from string content.
-   *  Non-noise means it is not a word in the noiseWords which have
-   *  been added to this object.  Normalized means that words are
-   *  lower-cased, have been stemmed and all non-alphabetic characters
-   *  matching regex [^a-z] have been removed.
-   */
-  words(content) {
-    //@TODO
-    return [];
-  }
+    /** Return array of non-noise normalized words from string content.
+     *  Non-noise means it is not a word in the noiseWords which have
+     *  been added to this object.  Normalized means that words are
+     *  lower-cased, have been stemmed and all non-alphabetic characters
+     *  matching regex [^a-z] have been removed.
+     */
+    words(content) {
+        let words = normalize(content);
+        for(let word of set){
+            words.replace(word, '');
+        }
+        let temp = words.split(' ');
+        return temp;
+    }
 
-  /** Add all normalized words in noiseWords string to this as
-   *  noise words. 
-   */
-  addNoiseWords(noiseWords) {
-    //@TODO    			      			    
-  }
+    /** Add all normalized words in noiseWords string to this as
+     *  noise words.
+     */
+    addNoiseWords(noiseWords) {
+        set = new Set(noiseWords.split(/\n/));
+    }
 
-  /** Add document named by string name with specified content to this
-   *  instance. Update index in this with all non-noise normalized
-   *  words in content string.
-   */ 
-  addContent(name, content) {
-    //@TODO	   
-  }
+    /** Add document named by string name with specified content to this
+     *  instance. Update index in this with all non-noise normalized
+     *  words in content string.
+     */
+    addContent(name, content) {
+        this.contents.set(name, content);
+    }
 
-  /** Given a list of normalized, non-noise words search terms, 
-   *  return a list of Result's  which specify the matching documents.  
-   *  Each Result object contains the following properties:
-   *     name:  the name of the document.
-   *     score: the total number of occurrences of the search terms in the
-   *            document.
-   *     lines: A string consisting the lines containing the earliest
-   *            occurrence of the search terms within the document.  Note
-   *            that if a line contains multiple search terms, then it will
-   *            occur only once in lines.
-   *  The Result's list must be sorted in non-ascending order by score.
-   *  Results which have the same score are sorted by the document name
-   *  in lexicographical ascending order.
-   *
-   */
-  find(terms) {
-    //@TODO
-    return [];
-  }
+    /** Given a list of normalized, non-noise words search terms,
+     *  return a list of Result's  which specify the matching documents.
+     *  Each Result object contains the following properties:
+     *     name:  the name of the document.
+     *     score: the total number of occurrences of the search terms in the
+     *            document.
+     *     lines: A string consisting the lines containing the earliest
+     *            occurrence of the search terms within the document.  Note
+     *            that if a line contains multiple search terms, then it will
+     *            occur only once in lines.
+     *  The Result's list must be sorted in non-ascending order by score.
+     *  Results which have the same score are sorted by the document name
+     *  in lexicographical ascending order.
+     *
+     */
+    find(terms) {
+        //@TODO
+        return [];
+    }
 
-  /** Given a text string, return a ordered list of all completions of
-   *  the last word in text.  Returns [] if the last char in text is
-   *  not alphabetic.
-   */
-  complete(text) {
-    //@TODO
-    return [];
-  }
+    /** Given a text string, return a ordered list of all completions of
+     *  the last word in text.  Returns [] if the last char in text is
+     *  not alphabetic.
+     */
+    complete(text) {
+        //@TODO
+        return [];
+    }
 
-  
+
 } //class DocFinder
 
 module.exports = DocFinder;
@@ -72,15 +77,15 @@ module.exports = DocFinder;
 /** Regex used for extracting words as maximal non-space sequences. */
 const WORD_REGEX = /\S+/g;
 
-/** A simple class which packages together the result for a 
+/** A simple class which packages together the result for a
  *  document search as documented above in DocFinder.find().
- */ 
+ */
 class Result {
-  constructor(name, score, lines) {
-    this.name = name; this.score = score; this.lines = lines;
-  }
+    constructor(name, score, lines) {
+        this.name = name; this.score = score; this.lines = lines;
+    }
 
-  toString() { return `${this.name}: ${this.score}\n${this.lines}`; }
+    toString() { return `${this.name}: ${this.score}\n${this.lines}`; }
 }
 
 /** Compare result1 with result2: higher scores compare lower; if
@@ -88,21 +93,21 @@ class Result {
  *  lower.
  */
 function compareResults(result1, result2) {
-  return (result2.score - result1.score) ||
-    result1.name.localeCompare(result2.name);
+    return (result2.score - result1.score) ||
+        result1.name.localeCompare(result2.name);
 }
 
 /** Normalize word by stem'ing it, removing all non-alphabetic
  *  characters and converting to lowercase.
  */
 function normalize(word) {
-  return stem(word.toLowerCase()).replace(/[^a-z]/g, '');
+    return stem(word.toLowerCase()).replace(/[^a-z]/g, '');
 }
 
 /** Place-holder for stemming a word before normalization; this
  *  implementation merely removes 's suffixes.
  */
 function stem(word) {
-  return word.replace(/\'s$/, '');
+    return word.replace(/\'s$/, '');
 }
 
