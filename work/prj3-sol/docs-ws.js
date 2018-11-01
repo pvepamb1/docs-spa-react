3eb0540cf81eccc244b1ce3a299f32e51c8cab78
Prasenna Venkatesh Vepambedu <pvepamb1@CS580w-pvepamb1.csvb.local>
2018-11-01 19:30:08 -0400

    started prj3

---
+++ work/prj3-sol/docs-ws.js
@@ -0,0 +1,88 @@
+'use strict';
+
+const cors = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const process = require('process');
+const url = require('url');
+const queryString = require('querystring');
+
+const OK = 200;
+const CREATED = 201;
+const BAD_REQUEST = 400;
+const NOT_FOUND = 404;
+const CONFLICT = 409;
+const SERVER_ERROR = 500;
+
+
+//Main URLs
+const DOCS = '/docs';
+const COMPLETIONS = '/completions';
+
+//Default value for count parameter
+const COUNT = 5;
+
+/** Listen on port for incoming requests.  Use docFinder instance
+ *  of DocFinder to access document collection methods.
+ */
+function serve(port, docFinder) {
+  const app = express();
+  app.locals.port = port;
+  app.locals.finder = docFinder;
+  setupRoutes(app);
+  const server = app.listen(port, async function() {
+    console.log(`PID ${process.pid} listening on port ${port}`);
+  });
+  return server;
+}
+
+module.exports = { serve };
+
+function setupRoutes(app) {
+  app.use(cors());            //for security workaround in future projects
+  app.use(bodyParser.json()); //all incoming bodies are JSON
+
+  //@TODO: add routes for required 4 services
+
+  app.use(doErrors()); //must be last; setup for server errors   
+}
+
+//@TODO: add handler creation functions called by route setup
+//routine for each individual web service.  Note that each
+//returned handler should be wrapped using errorWrap() to
+//ensure that any internal errors are handled reasonably.
+
+/** Return error handler which ensures a server error results in nice
+ *  JSON sent back to client with details logged on console.
+ */ 
+function doErrors(app) {
+  return async function(err, req, res, next) {
+    res.status(SERVER_ERROR);
+    res.json({ code: 'SERVER_ERROR', message: err.message });
+    console.error(err);
+  };
+}
+
+/** Set up error handling for handler by wrapping it in a 
+ *  try-catch with chaining to error handler on error.
+ */
+function errorWrap(handler) {
+  return async (req, res, next) => {
+    try {
+      await handler(req, res, next);
+    }
+    catch (err) {
+      next(err);
+    }
+  };
+}
+  
+
+/** Return base URL of req for path.
+ *  Useful for building links; Example call: baseUrl(req, DOCS)
+ */
+function baseUrl(req, path='/') {
+  const port = req.app.locals.port;
+  const url = `${req.protocol}://${req.hostname}:${port}${path}`;
+  return url;
+}
