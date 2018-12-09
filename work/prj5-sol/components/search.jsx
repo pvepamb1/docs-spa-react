b806c46b934551502e92088d1ccc7f58cb08f7cc
Prasenna Venkatesh Vepambedu <pvepamb1@CS580w-pvepamb1.csvb.local>
2018-12-08 20:52:40 -0500

    Started prj5

---
+++ work/prj5-sol/components/search.jsx
@@ -0,0 +1,30 @@
+//-*- mode: rjsx-mode;
+
+'use strict';
+
+const React = require('react');
+
+class Search extends React.Component {
+
+  /** called with properties:
+   *  app: An instance of the overall app.  Note that props.app.ws
+   *       will return an instance of the web services wrapper and
+   *       props.app.setContentName(name) will set name of document
+   *       in content tab to name and switch to content tab.
+   */
+  constructor(props) {
+    super(props);
+    //@TODO
+  }
+
+  //@TODO
+
+
+  render() {
+    //@TODO
+    return "";
+  }
+
+}
+
+module.exports = Search;
